99d6cfb4e2b8e4babf597ea95cbfd8ed465ab610
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-07-25 15:48:12 -0300

    Add type definition for TypeScript

---
+++ index.d.ts
@@ -0,0 +1,35 @@
+import { Component, ReactNode } from 'react';
+import { StyleProp, TextStyle, ViewStyle } from 'react-native';
+
+declare type SwitchProps = {
+  onValueChange?: (value: boolean) => void;
+  disabled?: boolean;
+  activeText?: string;
+  inActiveText?: string;
+  backgroundActive?: string;
+  backgroundInactive?: string;
+  value?: boolean;
+  circleActiveColor?: string;
+  circleInActiveColor?: string;
+  circleSize?: number;
+  circleBorderActiveColor?: string;
+  circleBorderInactiveColor?: string;
+  activeTextStyle?: StyleProp<TextStyle>;
+  inactiveTextStyle?: StyleProp<TextStyle>;
+  containerStyle?: StyleProp<ViewStyle>;
+  barHeight?: number;
+  circleBorderWidth?: number;
+  renderInsideCircle?: () => ReactNode;
+  changeValueImmediately?: boolean;
+  innerCircleStyle?: StyleProp<ViewStyle>;
+  outerCircleStyle?: StyleProp<ViewStyle>;
+  renderActiveText?: boolean;
+  renderInActiveText?: boolean;
+  switchLeftPx?: number;
+  switchRightPx?: number;
+  switchWidthMultiplier?: number;
+};
+
+declare class Switch extends Component<SwitchProps> {}
+
+export { Switch, SwitchProps };
